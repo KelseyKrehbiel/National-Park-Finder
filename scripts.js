5232d5bb5191af7acffc92a4fee59246d0e0f3ed
KelseyKrehbiel <krehbielk@gmail.com>
2020-11-06 01:44:30 -0700

    Search works. Requirements met.

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const myKey = 'zURgoE4HavD2m6V0rTQlBQPLxf2dgjGFsjrvZTn2'
+
+function buildRequest(state,results){
+    console.log("building request");
+    let requestURL = "https://developer.nps.gov/api/v1/parks?";
+    /*
+    */
+    //API takes comma delimited string of states
+    requestURL += `stateCode=${state}&limit=${results}&api_key=${myKey}`;
+
+    console.log(requestURL);
+    getParks(requestURL)
+}
+
+function getParks(requestURL) {
+    fetch(requestURL)
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+        displayResults(responseJson.data)
+      })
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
+  
+  function displayResults(parkData) {
+    console.log(parkData);
+    $('.parks').remove();
+    let parkList = ""
+    for(let p = 0; p < parkData.length; p++){
+        parkList += `<p class="parks"><a href="${parkData[p].url}">${parkData[p].fullName}</a></p>
+                    <p class="parks">${parkData[p].description}</p><br>`
+    }
+    $('main').append(parkList);
+  }
+  
+  function watchForm() {
+      //console.log("submitted")
+    $('form').submit(event => {
+      event.preventDefault();
+      console.log($('#state-list').val());
+      console.log($('#result-size').val());
+      let state = $('#state-list').val();
+      let results = $('#result-size').val();
+      buildRequest(state,results);
+    });
+  }
+  
+  $(function() {
+    //wait for submission
+    watchForm();
+  });
